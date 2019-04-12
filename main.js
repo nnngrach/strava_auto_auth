75dd38211cde4e634d5d575ea5ce7a9b076102ba
nnngrach <nnngrach@gmail.com>
2019-04-12 17:14:13 +0300

    Working but don't sharing result

--- main.js
+++ main.js
@@ -10,46 +10,49 @@ Apify.main(async () => {
       console.log('Launching Puppeteer');
       const browser = await Apify.launchPuppeteer();
 
+
       console.log('Authoriztion on www.strava.com/login');
-      const page = await browser.newPage();
-      await page.setViewport({width: 1280, height: 1024});
-      await page.goto('https://www.strava.com/login', {waitUntil: 'networkidle2'});
-      await page.waitForSelector('form');
-      await page.type('input#email', input.email);
-      await page.type('input#password', input.password);
-      await page.waitFor(200);
-      await page.evaluate(()=>document
+      const page1 = await browser.newPage();
+      await page1.setViewport({width: 1280, height: 1024});
+      await page1.goto('https://www.strava.com/login', {waitUntil: 'networkidle2'});
+      await page1.waitForSelector('form');
+      await page1.type('input#email', input.email);
+      await page1.type('input#password', input.password);
+      await page1.waitFor(200);
+      await page1.evaluate(()=>document
         .querySelector('button#login-button')
         .click()
       );
-      await page.waitForNavigation();
+      await page1.waitForNavigation();
 
-      console.log('Success! User URL loaded');
 
-      // Grab a screenshot
-      // console.log('Saving screenshot...');
-      // const screenshotBuffer = await page.screenshot();
+      // console.log('Saving screenshot of User page...');
+      // const screenshotBuffer = await page1.screenshot();
       // await Apify.setValue('screenshot.png', screenshotBuffer, { contentType: 'image/png' });
       // const storeId = process.env.APIFY_DEFAULT_KEY_VALUE_STORE_ID;
       // console.log(`- https://api.apify.com/v2/key-value-stores/${storeId}/records/screenshot.png`)
 
 
-      console.log('Extracting cookies');
+      console.log('Extracting _strava4_session cookie');
+      const sessionFourCookie = await page1.cookies();
+      //console.log(sessionFourCookie);
+
+
+      console.log('Authoriztion on heatmap-external-a.strava.com/auth');
+      // Use cookies in other tab or browser
+      const page2 = await browser.newPage();
+      await page2.setCookie(...sessionFourCookie);
+      await page2.goto('https://heatmap-external-a.strava.com/auth'); // Opens page as logged user
+
+
+      console.log('Extracting CloudFront cookies');
+      const cloudfontCookie = await page2.cookies();
+      //console.log(cloudfontCookie);
 
 
 
-
-
-
-
-
-
-
-      console.log('Closing Puppeteer...');
-      await page.close();
+      console.log('Success! Closing Puppeteer');
       await browser.close();
 
-      return 'done';
-      console.log('Done');
-
+      return cloudfontCookie;
 });
